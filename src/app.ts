da689a0435d920d898b673e4179bca3db6e4a5a2
Niushae <fabiana.moreno.fm@gmail.com>
2025-09-12 13:18:18 -0400

    fix: setting origin to start with url

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: new RegExp(process.env.FRONTEND_URL!),
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
